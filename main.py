70397814a7e763f0cfcb9e5b60217b067bfee67b
Marcel <voiception@github.com>
2024-10-05 14:18:52 +0200

    Port changed for Render App service

--- main.py
+++ main.py
@@ -94,4 +94,4 @@ def predict(request: InferenceBody) -> dict:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
